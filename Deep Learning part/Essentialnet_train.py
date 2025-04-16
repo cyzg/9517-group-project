e26948ffb5cb0c09a5ca14ce19d9589729304522
eleana0622 <130815303+eleana0622@users.noreply.github.com>
2025-04-17 00:27:36 +1000

    Update Essentialnet_train.py

--- Deep Learning part/Essentialnet_train.py
+++ Deep Learning part/Essentialnet_train.py
@@ -51,7 +51,7 @@ criterion = nn.CrossEntropyLoss()
 optimizer = optim.Adam(model.parameters(), lr=0.0001)
 
 # Strat training
-for epoch in range(12):
+for epoch in range(12): #change the number of epoch you wanna train
     print(f"start  {epoch+1}  epoch")
     model.train()
     total_loss = 0
